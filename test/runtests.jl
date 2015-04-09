30f480e74898ec61f847a141f391d705dbd5bcd9
Steve Pallone <snp32@cornell.edu>
2015-04-09 17:54:52 -0400

    adjusted tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using RandomStreams
 using Base.Test
 
 generator = MRG32k3aGen([4,5,6,7,8,9])
-@test MRG32k3aGen.nextSeed == [4,5,6,7,8,9]
+@test all(generator.nextSeed .== [4,5,6,7,8,9])
 
 rng1 = next_stream(generator)
 rng2 = next_stream(generator)
