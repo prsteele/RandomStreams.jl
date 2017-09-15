1977b622960b462f427e5249a11348347c7d46e3
fbastin <fabian.bastin@gmail.com>
2017-09-15 18:06:43 -0400

    Update RandomStreams.jl
    Update the type definition according Julia 0.6.0 standards.

--- src/RandomStreams.jl
+++ src/RandomStreams.jl
@@ -5,7 +5,7 @@ import Base: rand,srand,show
 
 export MRG32k3a, MRG32k3aGen, next_stream, next_substream!, reset_stream!, reset_substream!, get_state, advance_state!
 
-abstract AbstractRNGStream
+abstract type AbstractRNGStream end
 
 include("mrg32k3a.jl")
 include("mrg32k3a_types.jl")
